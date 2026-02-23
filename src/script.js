09d626a435fba56f2ac52359c590e5c56674d2fb
Diogo Teixeira <diteixeira04@gmail.com>
2026-02-23 12:20:16 +0000

    new projects added

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   // --- Projetos ---
   const projetos = [
-    { nome: "Checkers", linguagens: ["Kotlin", "Compose"], repo: "https://github.com/DiogoTeixeira3/Checkers", pdf: "files/Checkers.pdf", img: "files/checkers.png" },
-    { nome: "Padel Manager", linguagens: ["Kotlin", "SQL", "HTML", "CSS", "JavaScript"], repo: "https://github.com/DiogoTeixeira3/projeto2", pdf: "files/projeto2.pdf", img: "files/PadelManager.png" },
-    { nome: "Projeto 3", linguagens: ["Python", "Flask"], repo: "https://github.com/DiogoTeixeira3/projeto3", pdf: "files/projeto3.pdf", img: "files/projeto3.png" },
-    { nome: "Projeto 4", linguagens: ["C", "Assembly"], repo: "https://github.com/DiogoTeixeira3/projeto4", pdf: "files/projeto4.pdf", img: "files/projeto4.png" },
-    { nome: "Projeto 5", linguagens: ["React", "Node.js"], repo: "https://github.com/DiogoTeixeira3/projeto5", pdf: "files/projeto5.pdf", img: "files/projeto5.png" },
-    { nome: "Connect Four", linguagens: ["Kotlin"], repo: "https://github.com/DiogoTeixeira3/ConnectFour.git", pdf: "files/ConnectedFour.pdf", img: "files/ConnectFour.png" }
+    { nome: "Checkers", linguagens: ["Kotlin", "Compose"], repo: "https://github.com/DiogoTeixeira3/Checkers", pdf: "../files/Checkers.pdf", img: "../files/checkers.png" },
+    { nome: "Padel Manager Website", linguagens: ["Kotlin", "SQL", "HTML", "CSS", "JavaScript"], repo: "https://github.com/DiogoTeixeira3/Padel-Manager.git", pdf: "../files/Padel-Manager.pdf", img: "../files/padel.png" },
+    { nome: "Chelas Poker Dice Website", linguagens: ["React", "Spring", "TypeScript", "Kotlin", "Vite", "CSS"], repo: "https://github.com/DiogoTeixeira3/Chelas-Poker-Dice-Website.git", pdf: "../files/project-requirements.pdf", img: "../files/ChelasPokerDice.png" },
+    { nome: "Chelas Poker Dice Android App", linguagens: ["Kotlin", "Ngrok", "Android Studio"], repo: "https://github.com/DiogoTeixeira3/Chelas-Poker-Dice-App.git", pdf: "../files/ChelasPokerDiceApp.pdf", img: "../files/ChelasPokerDice.png" },
+    { nome: "Projeto 5", linguagens: ["React", "Node.js"], repo: "https://github.com/DiogoTeixeira3/projeto5", pdf: "../files/projeto5.pdf", img: "../files/projeto5.png" },
+    { nome: "Connect Four", linguagens: ["Kotlin"], repo: "https://github.com/DiogoTeixeira3/ConnectFour.git", pdf: "../files/ConnectedFour.pdf", img: "../files/ConnectFour.png" }
   ];
 
   const container = document.getElementById("projetos-container");
